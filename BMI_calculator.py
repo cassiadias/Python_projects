d199ba0880bc5e34bdfc2685779288f8c4d1be03
Cassia Dias <lbwork1020@gmail.com>
2021-05-19 16:04:43 -0300

    up

--- BMI_calculator.py
+++ BMI_calculator.py
@@ -1,4 +1,14 @@
-height = input("Enter your height in m: ")
-weight = input("Enter your weight in kg: ")
-bmi = float(weight)/float(height)**2
-print(bmi)
+height = float(input("Enter your height in m: "))
+weight = float(input("Enter your weight in kg: "))
+bmi = round(weight/height**2, 2)
+if bmi <= 18.5:
+    print(f"Your BMI is {bmi}, you are underweight")
+elif bmi >= 18.5 and bmi < 25:
+    print(f"Your BMI is {bmi},you have a normal weight")
+elif bmi >25 and bmi <=30:
+    print (f"Your BMI is {bmi},you are overweight")
+elif bmi >30 and bmi <=35:
+    print(f"Your BMI is {bmi},you are obese")
+else:
+    print(f"Your BMI is {bmi},you are clinically obese")
+
