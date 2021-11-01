d1bb4ff395f5850ea9ee71afbd191ef246c80f2b
Cassia Dias <lbwork1020@gmail.com>
2021-10-31 22:18:16 -0300

    up

---
+++ rollercoaster.py
@@ -0,0 +1,13 @@
+print("Welcome to Star rollercoaster!")
+age = int(input("What is your age?\n"))
+if age >= 18:
+    bill = 12
+    print("Youth tickets are 12.")
+elif age <= 12:
+    bill = 6
+    print("Child tickets are 6.")
+
+wants_photo = input("Do you want your photo taken? Y or N\n")
+if wants_photo == "Y":
+    bill += 3
+print(f"Your final bill is ${bill}\nHave fun!")
