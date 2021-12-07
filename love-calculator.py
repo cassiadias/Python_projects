e0694ee5e6d9f88116b91fbd994026d7bc0c9d7a
Cassia Dias <lbwork1020@gmail.com>
2021-12-06 21:57:45 -0300

    up

--- love-calculator.py
+++ love-calculator.py
@@ -20,7 +20,7 @@ love_score = int(str(true) + str(love))
 if (love_score < 10) or (love_score > 90):
     print(f"Your score love is {love_score}, you are go together like coke and mentos")
 elif (love_score >= 40) and (love_score <= 50):
-    print(f"Your score love is {love_score}, you are alright together <3<3")
+    print(f"Your score love is {love_score}, you are alright together")
 else:
     print(f"Your score is {love_score}")
 
